99b5964ea03e89d4f8f9f24ad41db5ee7aae2adb
Trevor Meadows <tlm04070@gmail.com>
2018-04-16 21:33:40 -0400

    added weather api, axios, and redux-promise

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const API_KEY = "0c0e57b2515ab41ff40f4dd725f2ca7b"
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = "FETCH_WEATHER";
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
